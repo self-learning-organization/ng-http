7bd608bc5e9958eec32cfa03d32648bd48592046
Chantal Neo <Chantal@hotmail.sg>
2020-03-16 17:22:53 +0800

    Response Interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,4 +1,5 @@
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEventType } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
@@ -6,6 +7,12 @@ export class AuthInterceptorService implements HttpInterceptor {
         // req.url = 'some url'; // This won't work because req is immutable, add it into req.clone's object input
         console.log(req.url);
         const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')}); // url: 'some-new-url'
-        return next.handle(modifiedRequest); // If you don't return next handle and pass the request, then the request will not continue and therefore causing app to break
+        return next.handle(modifiedRequest).pipe(tap(event => { // Instead of tap, I can also use other operators like map and even transform the response 
+            console.log(event);
+            if (event.type === HttpEventType.Response) {
+                console.log('Response arrived, body data: ');
+                console.log(event.body);
+            }
+        })); 
     }
 }
