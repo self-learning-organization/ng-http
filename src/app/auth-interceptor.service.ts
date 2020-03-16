9b23a3625bd278ad54be2c7a8102bc98a16c96d4
Chantal Neo <Chantal@hotmail.sg>
2020-03-16 17:31:21 +0800

    Multiple Interceptors

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,18 +1,10 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEventType } from '@angular/common/http';
-import { tap } from 'rxjs/operators';
+import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
 
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-        console.log('Request is on its way');
-        // req.url = 'some url'; // This won't work because req is immutable, add it into req.clone's object input
-        console.log(req.url);
-        const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')}); // url: 'some-new-url'
-        return next.handle(modifiedRequest).pipe(tap(event => { // Instead of tap, I can also use other operators like map and even transform the response 
-            console.log(event);
-            if (event.type === HttpEventType.Response) {
-                console.log('Response arrived, body data: ');
-                console.log(event.body);
-            }
-        })); 
+        const modifiedRequest = req.clone({
+            headers: req.headers.append('Auth', 'xyz')
+        }); // url: 'some-new-url'
+        return next.handle(modifiedRequest); 
     }
 }
