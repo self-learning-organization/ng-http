05a5034da635a3716c95d06d885e391dceebf09f
Chantal Neo <Chantal@hotmail.sg>
2020-03-16 17:15:19 +0800

    Manipulating Request Objects

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -3,6 +3,9 @@ import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http'
 export class AuthInterceptorService implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
         console.log('Request is on its way');
-        return next.handle(req); // If you don't return next handle and pass the request, then the request will not continue and therefore causing app to break
+        // req.url = 'some url'; // This won't work because req is immutable, add it into req.clone's object input
+        console.log(req.url);
+        const modifiedRequest = req.clone({headers: req.headers.append('Auth', 'xyz')}); // url: 'some-new-url'
+        return next.handle(modifiedRequest); // If you don't return next handle and pass the request, then the request will not continue and therefore causing app to break
     }
 }
