a901adcd429f790865ed5fa24ed5418771c8c70a
Chantal Neo <Chantal@hotmail.sg>
2020-03-16 13:55:24 +0800

    Showing a Loading Indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -40,6 +41,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http.get<{[key: string]: Post}>('https://angular-individual-learning.firebaseio.com/posts.json')
       .pipe(
         map((responseData) => {
@@ -53,6 +55,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe((posts) => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }
