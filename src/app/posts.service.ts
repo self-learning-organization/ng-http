2e1a73bc3b0c8f345cb8f4b4263a0fa8c8aa72a1
Chantal Neo <Chantal@hotmail.sg>
2020-03-16 16:38:02 +0800

    Adding Query Params

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { map, catchError } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 
@@ -29,12 +29,17 @@ export class PostsService {
     }
 
     fetchPosts() {
+        let searchParams = new HttpParams();
+        searchParams = searchParams.append('print', 'pretty');
+        searchParams = searchParams.append('custom', 'key');
         return this.http.get<{[key: string]: Post}>(
             'https://angular-individual-learning.firebaseio.com/posts.json', 
             {
                 headers: new HttpHeaders({
                     'Custom-Header': 'Hello'
-                })
+                }),
+                // params: new HttpParams().set('print', 'pretty')
+                params: searchParams
             }
             )
         .pipe(
